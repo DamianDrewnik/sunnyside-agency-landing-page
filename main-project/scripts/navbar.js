8b8c2a527c63cdc6f429b86173c2b24befee9194
Damian <larson98@gmail.com>
2021-10-05 16:36:09 +0200

    Mobile layout for 375px and v1 version of entire app

--- main-project/scripts/navbar.js
+++ main-project/scripts/navbar.js
@@ -1,10 +1,36 @@
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
+  const contact = document.querySelector(".contact");
 
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
+    contact.classList.toggle("contact--yellow");
   });
+
 };
 
 navSlide();
+
+/*
+.nav-links .contact{ -
+  text-transform: uppercase; -
+  background-color: white; -
+  color: black; -
+  border-radius: 34px; -
+  font-family: "Fraunces", serif; -
+  
+  }
+.nav-links {
+  display: flex;
+  /* justify-content: space-between;
+  width: 50%; 
+}
+
+.nav-links a {
+  color: hsl(0, 0%, 100%);
+  text-decoration: none;
+  padding: 1.5rem;
+  font-family: "Barlow", sans-serif;
+}
+*/
