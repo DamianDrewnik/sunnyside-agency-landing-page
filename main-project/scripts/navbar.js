e9c5fce43c27e96e221de8458a1da84ef9fb5ac8
Damian <larson98@gmail.com>
2021-10-22 11:58:41 +0200

    Code cleanup + README

--- main-project/scripts/navbar.js
+++ main-project/scripts/navbar.js
@@ -7,30 +7,6 @@ const navSlide = () => {
     nav.classList.toggle("nav-active");
     contact.classList.toggle("contact--yellow");
   });
-
 };
 
 navSlide();
-
-/*
-.nav-links .contact{ -
-  text-transform: uppercase; -
-  background-color: white; -
-  color: black; -
-  border-radius: 34px; -
-  font-family: "Fraunces", serif; -
-  
-  }
-.nav-links {
-  display: flex;
-  /* justify-content: space-between;
-  width: 50%; 
-}
-
-.nav-links a {
-  color: hsl(0, 0%, 100%);
-  text-decoration: none;
-  padding: 1.5rem;
-  font-family: "Barlow", sans-serif;
-}
-*/
